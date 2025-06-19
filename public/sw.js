62eef1aa25e301eb83c0276ed409a0e40ec38410
jwkim <joonoo1115@gmail.com>
2025-06-19 12:02:44 +0900

    update index.html

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,6 @@ self.addEventListener('push', event => {
   const data = event.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: 'icon.png'
+    icon: "./images/icon.png",
   });
 });
